fc271b7e23132a7db6a0e0697fcdb6c347b4397c
Lucas Zelaya <66498374+zelayalucass@users.noreply.github.com>
2023-11-06 00:23:13 -0300

    ARREGLO 2 DFE NICO APP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/auth/login/login.component';
 import { HttpClientModule } from '@angular/common/http';
 import { MatDialogModule } from '@angular/material/dialog';
 import { NavBarNotLoginComponent } from './shared/nav-bar-not-login/nav-bar-not-login.component';
@@ -9,13 +10,13 @@ import { FooterComponent } from './shared/footer/footer.component'
 import { AuthRoutingModule } from './components/auth/auth-routing.module';
 import { FormsModule } from '@angular/forms';
 import { AuthModule } from './components/auth/auth.module';
-import { LandingModule } from './components/landing/landing.module';
-import { SharedModule } from './shared/shared.module';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent
+
   ],
   imports: [
     BrowserModule,
