5f8d25ef863027160916387bfffa5aa6c87f102f
Lucas Zelaya <66498374+zelayalucass@users.noreply.github.com>
2023-11-07 01:17:05 -0300

    focus en login

--- src/app/components/auth/login/login.component.ts
+++ src/app/components/auth/login/login.component.ts
@@ -17,15 +17,38 @@ export class LoginComponent implements OnInit{
 
   public user:User = new User();
   public loginForm!: FormGroup; // Crea un FormGroup
+  public focusedField: string | null = null; 
 
   ngOnInit(): void {
     this.loginForm = this.formBuilder.group({
       email: [this.user.email, [Validators.required, Validators.email]],
-      password: [this.user.password, [Validators.required, Validators.minLength(3)]]
+      password: [this.user.password, [Validators.required]]
     });
+
+    // Configura un listener para el campo de correo electrónico
+    this.loginForm.get('email')?.valueChanges.subscribe((email) => {
+      if (!email) {
+        // Borra los errores de ambos campos si el campo de correo electrónico está vacío
+        this.loginForm.get('email')?.setErrors(null);
+        this.loginForm.get('password')?.setErrors(null);
+      }
+    });
+
+    // Configura un listener para el evento focusout del campo de correo electrónico
+    this.loginForm.get('email')?.valueChanges.subscribe(() => {
+      this.loginForm.get('email')?.updateValueAndValidity(); // Actualiza la validación en focusout
+    });
+
+    
   }
 
+    // Función para verificar si el campo de contraseña está lleno
+ 
   constructor(private dialog: MatDialog, private router:Router, private auth: AuthService, private api:ApiService,  private formBuilder: FormBuilder) {}
+ 
+  hasError(controlName: string, errorName: string): boolean {
+    return this.loginForm.get(controlName)?.hasError(errorName) || false;
+  }
 
   openRegisterDialog(): void {
     const dialogRef = this.dialog.open(RegisterComponent, {
@@ -42,8 +65,6 @@ export class LoginComponent implements OnInit{
     this.router.navigate(['/auth/register'])
   }
   
-
-
   public async checkAuthLogin()
   {
      try
