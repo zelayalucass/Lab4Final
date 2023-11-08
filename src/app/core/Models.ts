8943d7b394115dd2dbf039c374e8ef76ef11932f
Nicolas Estigarribia <72633138+nicolasestigarribia@users.noreply.github.com>
2023-11-08 17:18:07 -0300

    Se acamodaron las referencias de los componentes

--- src/app/core/Models.ts
+++ src/app/core/Models.ts
@@ -3,7 +3,7 @@ import { ICinema } from "./Interface";
 import { IMovie } from "./Interface";
 import { ITicket } from "./Interface";
 import { IShowtime } from "./Interface";
-
+import { OriginalLanguage } from "./original-language";
 export class User implements IUser
 {
     id: number | null;
@@ -34,24 +34,37 @@ export class Cinema implements ICinema {
 }
 
  export class Movie implements IMovie {
-    idMovie: number | null;
-    titulo: string | null;
-    duracion: string | null;
-    genero: string | null;
-    idioma: string | null;
-    año: string | null;
-    descripcion: string | null;
-    imagen: string | null;
+    adult:             boolean | null;
+    backdrop_path:     string;
+    genre_ids:         number[];
+    id:                number;
+    original_language: OriginalLanguage;
+    original_title:    string;
+    overview:          string;
+    popularity:        number;
+    poster_path:       string;
+    release_date:      Date;
+    title:             string;
+    video:             boolean;
+    vote_average:      number;
+    vote_count:        number;
 
-    constructor(movie?: Movie){
-        this.idMovie = movie?.idMovie == undefined ? null : movie.idMovie;
-        this.titulo = movie?.titulo == undefined ? null : movie.titulo;
-        this.duracion = movie?.duracion == undefined ? null : movie.duracion;
-        this.genero = movie?.genero == undefined ? null : movie.genero;
-        this.idioma = movie?.idioma == undefined ? null : movie.idioma;
-        this.año = movie?.año == undefined ? null : movie.año;
-        this.descripcion = movie?.descripcion == undefined ? null : movie.descripcion;
-        this.imagen = movie?.imagen == undefined ? null : movie.imagen;
+    constructor(movie?: Movie)
+    {
+        this.adult = movie?.adult == undefined ? null : movie.adult;
+        this.backdrop_path = movie?.backdrop_path == undefined ? "" : movie.backdrop_path;
+        this.genre_ids = movie?.genre_ids == undefined ? [] : movie.genre_ids;
+        this.id = movie?.id == undefined ? 0 : movie.id;
+        this.original_language = movie?.original_language == undefined ? OriginalLanguage.None : movie.original_language;
+        this.original_title = movie?.original_title == undefined ? "" : movie.original_title;
+        this.overview = movie?.overview == undefined ? "" : movie.overview;
+        this.popularity = movie?.popularity == undefined ? 0 : movie.popularity;
+        this.release_date = movie?.release_date == undefined ? new Date() : movie.release_date;
+        this.poster_path = movie?.poster_path == undefined ? "" : movie?.poster_path;
+        this.title = movie?.title == undefined ? "" : movie.title;
+        this.video = movie?.video == undefined ? false : movie.video;
+        this.vote_average = movie?.vote_average == undefined ? 0 : movie.vote_average;
+        this.vote_count = movie?.vote_count == undefined ? 0 : movie.vote_count;
     }
  }
 
