11b529b3d17f7eaf7882f079b818bac4d9fcf538
Pablo Emanuel Morales <83431035+pablostereo@users.noreply.github.com>
2023-11-14 21:15:37 -0300

    User Module corregido

--- src/app/components/users/users.module.ts
+++ src/app/components/users/users.module.ts
@@ -1,22 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { UsersRoutingModule } from './users-routing.module';
 import { HomeUserComponent } from './home-user/home-user.component';
-import { DetailsUserComponent } from './details-user/details-user.component';
-import { FooterComponent } from 'src/app/shared/footer/footer.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 
 
 @NgModule({
   declarations: [
     HomeUserComponent,
-    FooterComponent
   ],
   imports: [
     CommonModule,
     UsersRoutingModule,
-    //SharedModule
+    SharedModule
   ]
 })
 export class UsersModule { }
