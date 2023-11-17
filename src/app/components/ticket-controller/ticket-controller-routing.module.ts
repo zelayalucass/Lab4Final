58c28affe354474ba31ec96cdfb9d22c8dfaa99b
Pablo Emanuel Morales <83431035+pablostereo@users.noreply.github.com>
2023-11-16 22:40:00 -0300

    add list ticket

--- src/app/components/ticket-controller/ticket-controller-routing.module.ts
+++ src/app/components/ticket-controller/ticket-controller-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeTicketComponent } from './home-ticket/home-ticket.component';
+import { ListTicketComponent } from './list-ticket/list-ticket.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"home",
+    component:HomeTicketComponent
+  },
+  {
+    path:"list",
+    component:ListTicketComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
